94a7d5f2b34f85ae470ecdf736caea3473794274
DominikZurawski <38438473+DominikZurawski@users.noreply.github.com>
2025-01-30 16:12:48 +0100

    django_rest_framework

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,8 +16,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from rest_framework import routers
+from movies.views import MovieViewSet, ActionViewSet, ComedyViewSet
+from django.conf.urls.static import static
+from django.conf import settings
+
+router = routers.SimpleRouter()
+router.register('movies', MovieViewSet, basename='movies_router')
+router.register('action', ActionViewSet, basename='genre_router')
+router.register('comedy',ComedyViewSet, basename='movieview_router')
 
 urlpatterns = [
+    path('', include(router.urls)),
     path('admin/', admin.site.urls),
-    path('app/', include('app.urls'))
-]
+]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
